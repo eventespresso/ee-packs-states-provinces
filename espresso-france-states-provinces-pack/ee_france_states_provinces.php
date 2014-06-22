7d9340bb841bd85658d76f18a88ed760d1fcb2dc
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2014-06-21 21:36:42 -0400

    Fix typos for particular and warranty.

--- espresso-france-states-provinces-pack/ee_france_states_provinces.php
+++ espresso-france-states-provinces-pack/ee_france_states_provinces.php
@@ -13,8 +13,8 @@
   published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WFRRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PFRTICULFR PURPOSE.See the
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
