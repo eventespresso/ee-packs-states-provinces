ed6d82bf3ef37f7b2f736fec1cf336d82bbd56a0
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2015-07-20 20:06:49 -0400

    Apply standardization changes for #33. #36

--- espresso-indonesia-states-provinces-pack/ee_indonesia_states_provinces.php
+++ espresso-indonesia-states-provinces-pack/ee_indonesia_states_provinces.php
@@ -1,8 +1,8 @@
 <?php
 /*
-  Plugin Name: Event Espresso - States/Provinces Pack: indonesia
+  Plugin Name: Event Espresso - States/Provinces Pack: Indonesia
   Plugin URI: http://www.eventespresso.com
-  Description: The Event Espresso New Addon adds indonesia CountIDs to Event Espresso 4
+  Description: This plugins adds states/provinces for the country of Indonesia to Event Espresso 4.
   Version: 1.0
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
@@ -22,45 +22,51 @@ function ee_indonesia_states_provinces_activate() {
   $installed = get_option( 'ee_indonesia_states_provinces_added');
   
   if ($installed) return;
+  
   global $wpdb;
+  
   $sql = "INSERT INTO `" . $wpdb->prefix . "esp_state` (`CNT_ISO`, `STA_abbrev`, `STA_name`, `STA_active`) VALUES
-   ('ID','ID-AC','Aceh',1),
-('ID','ID-BA','Bali',1),
-('ID','ID-BB','Bangka Belitung',1),
-('ID','ID-BT','Banten',1),
-('ID','ID-BE','Bengkulu',1),
-('ID','ID-GO','Gorontalo',1),
-('ID','ID-JA','Jambi',1),
-('ID','ID-JB','Jawa Barat',1),
-('ID','ID-JT','Jawa Tengah',1),
-('ID','ID-JI','Jawa Timur',1),
-('ID','ID-KB','Kalimantan Barat',1),
-('ID','ID-KS','Kalimantan Selatan',1),
-('ID','ID-KT','Kalimantan Tengah',1),
-('ID','ID-KI','Kalimantan Timur',1),
-('ID','ID-KU','Kalimantan Utara',1),
-('ID','ID-KR','Kepulauan Riau',1),
-('ID','ID-LA','Lampung',1),
-('ID','ID-MA','Maluku',1),
-('ID','ID-MU','Maluku Utara',1),
-('ID','ID-NB','Nusa Tenggara Barat',1),
-('ID','ID-NT','Nusa Tenggara Timur',1),
-('ID','ID-PA','Papua',1),
-('ID','ID-PB','Papua Barat',1),
-('ID','ID-RI','Riau',1),
-('ID','ID-SR','Sulawesi Barat',1),
-('ID','ID-SN','Sulawesi Selatan',1),
-('ID','ID-ST','Sulawesi Tengah',1),
-('ID','ID-SG','Sulawesi Tenggara',1),
-('ID','ID-SA','Sulawesi Utara',1),
-('ID','ID-SB','Sumatera Barat',1),
-('ID','ID-SS','Sumatera Selatan',1),
-('ID','ID-SU','Sumatera Utara',1),
-('ID','ID-JK','Jakarta Raya',1),
-('ID','ID-YO','Yogyakarta',1);";
+   ('ID', 'ID-AC', 'Aceh', 1),
+   ('ID', 'ID-BA', 'Bali', 1),
+   ('ID', 'ID-BB', 'Bangka Belitung', 1),
+   ('ID', 'ID-BT', 'Banten', 1),
+   ('ID', 'ID-BE', 'Bengkulu', 1),
+   ('ID', 'ID-GO', 'Gorontalo', 1),
+   ('ID', 'ID-JA', 'Jambi', 1),
+   ('ID', 'ID-JB', 'Jawa Barat', 1),
+   ('ID', 'ID-JT', 'Jawa Tengah', 1),
+   ('ID', 'ID-JI', 'Jawa Timur', 1),
+   ('ID', 'ID-KB', 'Kalimantan Barat', 1),
+   ('ID', 'ID-KS', 'Kalimantan Selatan', 1),
+   ('ID', 'ID-KT', 'Kalimantan Tengah', 1),
+   ('ID', 'ID-KI', 'Kalimantan Timur', 1),
+   ('ID', 'ID-KU', 'Kalimantan Utara', 1),
+   ('ID', 'ID-KR', 'Kepulauan Riau', 1),
+   ('ID', 'ID-LA', 'Lampung', 1),
+   ('ID', 'ID-MA', 'Maluku', 1),
+   ('ID', 'ID-MU', 'Maluku Utara', 1),
+   ('ID', 'ID-NB', 'Nusa Tenggara Barat', 1),
+   ('ID', 'ID-NT', 'Nusa Tenggara Timur', 1),
+   ('ID', 'ID-PA', 'Papua', 1),
+   ('ID', 'ID-PB', 'Papua Barat', 1),
+   ('ID', 'ID-RI', 'Riau', 1),
+   ('ID', 'ID-SR', 'Sulawesi Barat', 1),
+   ('ID', 'ID-SN', 'Sulawesi Selatan', 1),
+   ('ID', 'ID-ST', 'Sulawesi Tengah', 1),
+   ('ID', 'ID-SG', 'Sulawesi Tenggara', 1),
+   ('ID', 'ID-SA', 'Sulawesi Utara', 1),
+   ('ID', 'ID-SB', 'Sumatera Barat', 1),
+   ('ID', 'ID-SS', 'Sumatera Selatan', 1),
+   ('ID', 'ID-SU', 'Sumatera Utara', 1),
+   ('ID', 'ID-JK', 'Jakarta Raya', 1),
+   ('ID', 'ID-YO', 'Yogyakarta', 1);";
+   
   if ($wpdb->query($sql)){
     
     add_option( 'ee_indonesia_states_provinces_added', 1, '', 'no' );
+  
   }
+  
 }
+
 register_activation_hook( __FILE__, 'ee_indonesia_states_provinces_activate' );
